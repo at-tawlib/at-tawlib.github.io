b11b49a7fba6fc29642e2f11905bdbd5fe22fc59
Abdul-Fatahu Hardi <azindodev@gmail.com>
2024-10-08 07:46:55 +0000

    Add and implement skills sections

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// For dynamic rendering of typed text
+// For dynamic rendering of typed text animation
 var typed = new Typed(".typing-text", {
   strings: [
     "frontend development",
@@ -10,4 +10,34 @@ var typed = new Typed(".typing-text", {
   typeSpeed: 50,
   backSpeed: 25,
   backDelay: 500,
+});
+
+async function fetchData(type = "skills") {
+  let response;
+  type === "skills"
+    ? (response = await fetch("skills.json"))
+    : (response = await fetch("./projects/projects.json"));
+
+  const data = await response.json();
+  return data;
+}
+
+function showSkills(skills) {
+  let skillsContainer = document.getElementById("skillsContainer");
+  let skillHTML = "";
+  skills.forEach((skill) => {
+    skillHTML += `
+    <div class="bar">
+      <div class="info">
+        <img src=${skill.icon} alt="skill" />
+        <span>${skill.name}</span>
+      </div>
+    </div>`;
+  });
+  skillsContainer.innerHTML = skillHTML;
+}
+
+// get skills from file and load it in webpage
+fetchData().then((data) => {
+  showSkills(data);
 });

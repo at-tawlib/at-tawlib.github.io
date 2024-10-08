e40fc2ed7f379d5f2b2458b4248555649cc7e7f8
Abdul-Fatahu Hardi <azindodev@gmail.com>
2024-10-08 07:03:25 +0000

    Add typed animation and remove CO

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-// AOS.init();
-document.addEventListener("DOMContentLoaded", function () {
-  AOS.init({
-    duration: 1000, // Animation duration in milliseconds
-    once: false, // Whether animation should happen only once or every time you scroll
-  });
+// For dynamic rendering of typed text
+var typed = new Typed(".typing-text", {
+  strings: [
+    "frontend development",
+    "backend development",
+    "android app development",
+    "web development",
+  ],
+  loop: true,
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 500,
 });
-console.log("Hello world");
